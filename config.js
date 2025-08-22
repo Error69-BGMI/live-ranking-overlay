3340d82933509d4364ef105da58a00e71b3bf0a3
Akhil Surya <akhilsuray@gmail.com>
2025-08-22 22:25:46 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,2 @@
+// Replace with your Apps Script Web App URL (no trailing slash)
+window.SHEET_API = "https://script.google.com/macros/s/AKfycbzk0cczGJsMtdkAP1DTZMS-5vdeZmWyQ7PhuwpfwXR7EYxxOkHYenY7Hh6knoN1YBsRwg/exec";
